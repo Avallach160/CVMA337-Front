cee65467dfb2e1beaefc9828a11d743066be45d2
Avallach160 <cbaird@kairyt.com>
2014-10-26 08:05:12 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cvma337FrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cvma337FrontApp
+ */
+angular.module('cvma337FrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
